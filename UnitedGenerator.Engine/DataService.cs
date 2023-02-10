59bf74f89b36bae5279b1631e8b891518040d85e
Brian Nors Jensen <bnj@cvation.com>
2023-02-10 14:56:36 +0100

    Very simple generation

---
+++ UnitedGenerator.Engine/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitedGenerator.Data;
+
+namespace UnitedGenerator.Engine
+{
+    public class DataService
+    {
+        public ISeason[] Seasons => DataFactory.Seasons;
+
+        public IBox[] Boxes => Seasons.SelectMany(x => x.Boxes).ToArray();
+
+        public IHero[] Heroes => Boxes.SelectMany(x => x.Heroes).ToArray();
+    }
+}
