7dd6c7fd425dd9f6bcfbb6dfcea5fc04ccbe8f80
Brian Nors Jensen <bnj@cvation.com>
2023-02-14 21:13:13 +0100

    Improved storage

--- UnitedGenerator/MyCollectionService.cs
+++ UnitedGenerator/MyCollectionService.cs
@@ -1,6 +1,7 @@
 ﻿using Blazored.LocalStorage;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnitedGenerator.Common;
 using UnitedGenerator.Engine;
 
@@ -15,27 +16,39 @@ namespace UnitedGenerator
             _storage = storage;
         }
 
-        public void SetAll(IBox box, bool included)
+        public void SetIncluded(IBox box, bool included)
         {
+            Write(box, included);
+
             foreach (var item in box.AllItems)
             {
-                SetIncluded(item, included);
+                Write(item, null);
             }
         }
 
         public void SetIncluded(IBoxItem item, bool included)
         {
-            if (included)
+            var boxState = ReadBox(item.Box);
+
+            if (boxState.HasValue)
             {
-                _storage.RemoveItem(item.Id);
+                Write(item.Box, null);
+
+                foreach (var child in item.Box.AllItems)
+                {
+                    Write(child, boxState);
+                }
             }
-            else
-            {
-                _storage.SetItem(item.Id, included);
-            }
+
+            Write(item, included);
         }
 
         public bool IsIncluded(IBoxItem item)
+        {
+            return ReadBox(item.Box) ?? ReadBoxItem(item);
+        }
+
+        private bool ReadBoxItem(IBoxItem item)
         {
             if (_storage.ContainKey(item.Id))
             {
@@ -43,6 +56,40 @@ namespace UnitedGenerator
             }
 
             return true;
+        }
+
+        private bool? ReadBox(IBox box)
+        {
+            if(_storage.ContainKey(box.Id))
+            {
+                return _storage.GetItem<bool>(box.Id);
+            }
+
+            return null;
+        }
+
+        private void Write(IBox box, bool? included)
+        {
+            if (included.HasValue)
+            {
+                _storage.SetItem(box.Id, included);
+            }
+            else
+            {
+                _storage.RemoveItem(box.Id);
+            }
+        }
+
+        private void Write(IBoxItem item, bool? included)
+        {
+            if (included.HasValue)
+            {
+                _storage.SetItem(item.Id, included);
+            }
+            else
+            {
+                _storage.RemoveItem(item.Id);
+            }
         }
     }
 }
