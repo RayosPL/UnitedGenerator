9e00e02f82560f7d6cd16c3057ca3fd440d49a1f
Brian Nors Jensen <bnj@cvation.com>
2023-02-15 13:49:14 +0100

    Details

---
+++ UnitedGenerator/UIExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnitedGenerator.Common;
+
+namespace UnitedGenerator
+{
+    public static class UIExtensions
+    {
+        public static string ToItemString(this IBoxItem item, IBoxItem parent)
+        {
+            if (item.Box.Id == parent.Box.Id)
+            {
+                return item.Name;
+            }
+            else
+            {
+                return $"{item.Name} ({item.Box.Name}, Season {item.Season.Number})";
+            }
+        }
+    }
+}
