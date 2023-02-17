724115abcb52e3de8d4d2fbac7e722bef646717a
Brian Nors Jensen <bnj@cvation.com>
2023-02-17 09:05:16 +0100

    Line breaks

--- UnitedGenerator/UIExtensions.cs
+++ UnitedGenerator/UIExtensions.cs
@@ -1,4 +1,6 @@
-﻿using UnitedGenerator.Common;
+﻿using Microsoft.AspNetCore.Components;
+using System.Runtime.ExceptionServices;
+using UnitedGenerator.Common;
 
 namespace UnitedGenerator
 {
@@ -14,6 +16,34 @@ namespace UnitedGenerator
             {
                 return $"{item.Name} ({item.Box.Name})";
             }
+        }
+
+        public static MarkupString Break(this string? text, int cutAfter = 20)
+        {
+            if (text is null)
+            {
+                return new MarkupString();
+            }
+
+            if (text.Length > cutAfter)
+            {
+                int index = text.IndexOf('(');
+
+                if (index <= 0)
+                {
+                    index = text.IndexOf(" ", cutAfter);
+                }
+
+                if (index > 0)
+                {
+                    string first = text.Substring(0, index);
+                    string last = text.Substring(index).Trim();
+
+                    return new MarkupString($"{first}<br />{last}");
+                }
+            }
+
+            return new MarkupString(text);
         }
     }
 }
